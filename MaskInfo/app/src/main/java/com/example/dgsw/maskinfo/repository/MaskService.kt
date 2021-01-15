98c6467c2fc3a628743528f811c2de69cd4f6e33
양윤재 <diddbswo04@dgsw.hs.kr>
2021-01-15 12:39:30 +0900

    Added RetroFitService

--- MaskInfo/app/src/main/java/com/example/dgsw/maskinfo/repository/MaskService.kt
+++ MaskInfo/app/src/main/java/com/example/dgsw/maskinfo/repository/MaskService.kt
@@ -1,13 +1,15 @@
 package com.example.dgsw.maskinfo.repository
 
+import com.example.dgsw.maskinfo.model.Store
 import com.example.dgsw.maskinfo.model.StoreInfo
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface MaskService {
     companion object {
         const val BASE_URL: String =
-            "https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94"
+            "https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/"
     }
 
     @GET("sample.json")
